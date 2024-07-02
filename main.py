cad98a00d68b464c5f553eb623b177ccbedcbf7f
burkhon-n <158993562+burkhon-n@users.noreply.github.com>
2024-07-02 18:11:51 +0500

    Update main.py

--- main.py
+++ main.py
@@ -79,4 +79,5 @@ def echo_all(message: types.Message):
     elif str(message.from_user.id) == ADMIN and not message.reply_to_message:
         bot.send_message(message.chat.id, "Reply to a message to send it to the user.")
 
-bot.infinity_polling()
+if __name__ == "__main__":
+    bot.polling()
