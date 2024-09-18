5d6d61c775717b1cc6477305a9fbe28a5fde4955
burkhon-n <158993562+burkhon-n@users.noreply.github.com>
2024-09-18 15:06:53 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 from telebot import types
 
-TOKEN = "7394051330:AAFVq1seQq2qAs6FS0eILvgBAWpMohMhkyc"
-ADMIN = "5025688887"
-GUEST = "684808707"
+TOKEN = "TOKEN"
+ADMIN = "admin_id"
+GUEST = "guest_id"
